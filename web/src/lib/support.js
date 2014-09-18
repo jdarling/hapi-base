8eb526d3f625099fb0ac0ae1b3ce177c55685096
Jeremy Darling <jeremy.darling@pearson.com>
2014-09-18 11:59:18 -0500

    Fix indention

--- web/src/lib/support.js
+++ web/src/lib/support.js
@@ -30,10 +30,10 @@ module.exports = {
   },
 
   paramByName: function(name, defaultValue) {
-      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-          results = regex.exec(location.search);
-      return results == null ? defaultValue : decodeURIComponent(results[1].replace(/\+/g, " "));
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? defaultValue : decodeURIComponent(results[1].replace(/\+/g, " "));
   },
 
   filterParams: function(filter, defaultValues){
